821123e6d28fc88e71129a037296b0c143ab73b3
zhiuxr <zhilwanmostafaei@gmail.com>
2024-02-29 10:56:36 +0330

    setting if and else statement

--- Logic Operators.py
+++ Logic Operators.py
@@ -1,2 +1,9 @@
 is_magician = False
 is_expert = True
+
+if is_magician == is_expert:
+    print('You Are a Master Magician')
+elif is_magician != is_expert:
+    print('At Least You\'re Getting There')
+else:
+    print('You Need Magic Powers')
